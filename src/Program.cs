68ee190815377ed86c13f24b2d31d4cb8572980d
Hakim Cassimally <hakim.cassimally@gmail.com>
2020-08-04 22:41:30 +0100

    Use cases 1-2
    Large chunk of commit. Have found this process very frustrating in C#
    (compared to Minesweeper Kata, which fell together nicely.)
    
    * `String.Split` has very different semantics with `\r\n` separator than
        expected
    * converting between datatypes still not great (I think mostly my lack
    of familiarity with the smoother idioms?)
    * Zip only accepts 2 lists
    * Failed attempt to use `yield return`, so instead doing mutation for
      the chunking.

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BankOCR
 {
-    class Program
+    public class OCR
     {
+        public static string rawDigits = @"
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|";
+
+        public static string[] digits = Chunk(rawDigits).ToArray();
+
+        public static Dictionary<string, int> digitsDictionary =
+            digits.Select((s, i) => (s, i)).ToDictionary(x => x.Item1, x => x.Item2);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static List<string> Chunk(string src)
+        {
+            string[] lines = src.Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var output = new List<string>();
+            while (lines.All(l => l.Length >= 3))
+            {
+                string head = String.Join('\n', lines.Select(l => l.Substring(0, 3)).ToArray());
+                output.Add(head);
+
+                lines = lines.Select(l => l.Substring(3)).ToArray();
+            }
+            return output;
+        }
+
+        public static string ReadDigits(string src)
+        {
+            var chunks = Chunk(src);
+            var ds = chunks.Select(c => digitsDictionary.ContainsKey(c) ?
+                                    digitsDictionary[c].ToString() : "?");
+            return String.Concat(ds.ToArray());
+        }
+        public static bool Checksum(string d)
+        {
+            int[] checks = d.Reverse()
+                        .Zip(Enumerable.Range(1,9))
+                        .Select(x => Int32.Parse(x.Item1.ToString()) * x.Item2)
+                        .ToArray();
+            return checks.Sum() % 11 == 0;
         }
     }
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using BankOCR;
+using System;
 
 namespace BankOCRTests
 {
@@ -9,10 +11,87 @@ namespace BankOCRTests
         {
         }
 
+        static string zero = @"
+ _ 
+| |
+|_|";
+        static string zeros = @"
+ _  _ 
+| || |
+|_||_|";
+
         [Test]
-        public void Test1()
+        public void BasicChunkTest()
         {
-            Assert.Pass();
+            var chunkedZeros = OCR.Chunk(zeros);
+            Assert.AreEqual(zero, "\n" + chunkedZeros[0]);
+            Assert.AreEqual(zero, "\n" + chunkedZeros[1]);
+            
+            Assert.AreEqual(OCR.digits[0], chunkedZeros[1]);
+        }
+        [Test]
+        public void DigitsDictTest()
+        {
+            Assert.AreEqual(0, OCR.digitsDictionary[OCR.digits[0]]);
+            Assert.AreEqual(3, OCR.digitsDictionary[OCR.digits[3]]);
+            Assert.AreEqual(9, OCR.digitsDictionary[OCR.digits[9]]);
+        }
+
+        [Test]
+        public void ReadDigitsTest()
+        {
+            Assert.AreEqual("0123456789", OCR.ReadDigits(OCR.rawDigits));
+            Assert.AreEqual("000000000", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+| || || || || || || || || |
+|_||_||_||_||_||_||_||_||_|"));
+            Assert.AreEqual("111111111", OCR.ReadDigits(@"
+                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |"));
+            Assert.AreEqual("222222222", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+|_ |_ |_ |_ |_ |_ |_ |_ |_ "));
+            Assert.AreEqual("333333333", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+ _| _| _| _| _| _| _| _| _|"));
+            Assert.AreEqual("444444444", OCR.ReadDigits(@"
+                           
+|_||_||_||_||_||_||_||_||_|
+  |  |  |  |  |  |  |  |  |"));
+            Assert.AreEqual("555555555", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+ _| _| _| _| _| _| _| _| _|"));
+            Assert.AreEqual("666666666", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+|_||_||_||_||_||_||_||_||_|"));
+            Assert.AreEqual("777777777", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |"));
+            Assert.AreEqual("888888888", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+|_||_||_||_||_||_||_||_||_|"));
+            Assert.AreEqual("999999999", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _  _ 
+|_||_||_||_||_||_||_||_||_|
+ _| _| _| _| _| _| _| _| _|"));
+            Assert.AreEqual("000000051", OCR.ReadDigits(@"
+ _  _  _  _  _  _  _  _    
+| || || || || || || ||_   |
+|_||_||_||_||_||_||_| _|  |"));
+    }
+
+        [Test]
+        public void CheckSumTest()
+        {
+            Assert.True(OCR.Checksum("457508000"));
+            Assert.False(OCR.Checksum("457508001"));
         }
     }
 }
