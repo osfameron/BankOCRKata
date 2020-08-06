31e2eac701bdfabf204412816c622804ca250773
Hakim Cassimally <hakim.cassimally@gmail.com>
2020-08-06 23:07:33 +0100

    mid refactor
    So, er, I realise I'm doing a lot of `public static` like I want to be
    doing FP rather than OO. That's fine for sketching out algorithms, but
    I'm getting up to speed with an OO language, so let's start to actually
    engage with the prevailing idioms...
    
    Refactoring into OCR (the line) and OCRDigit classes.
    Having done TDD throughout, I was unsure exactly how I wanted to
    structure this, and have just hacked at the source code instead of
    test-first. On reflection, this would be the ideal place to do test
    first, to sketch out the interface.
    
    Oh well, good learning experience I guess. Let's call it a "spike" this
    time, and remember to do better next time.
    
    Have taken it to a point where the obvious compiler errors are fixed in
    src, and the errors are now reported from the Tests, so next I'll
    rewrite those till I'm sure the structure works, and then fix remaining
    issues with code.

--- src/Program.cs
+++ src/Program.cs
@@ -7,18 +7,16 @@ namespace BankOCR
 {
     public class OCR
     {
-        public static string rawDigits = @"
- _     _  _     _  _  _  _  _ 
-| |  | _| _||_||_ |_   ||_||_|
-|_|  ||_  _|  | _||_|  ||_| _|";
+        public string Source { get; }
+        public OCRDigit[] Digits { get; }
 
-        public static string[] digits = Chunk(rawDigits).ToArray();
-
-        public static Dictionary<string, int> digitsDictionary =
-            digits.Select((s, i) => (s, i)).ToDictionary(x => x.Item1, x => x.Item2);
-
-        public static Dictionary<int, int> bitDictionary =
-            digits.Select((s, i) => (s, i)).ToDictionary(x => ToBitInt(ToBitArray(x.Item1)), x => x.Item2);
+        public OCR (string source)
+        {
+            Source = source;
+ 
+            var chunks = Chunk(source);
+            Digits = chunks.Select(c => new OCRDigit(c)).ToArray();  
+        }
 
         static void Main(string[] args)
         {
@@ -40,45 +38,71 @@ namespace BankOCR
             return output;
         }
 
+        public bool Checksum()
+        {
+            int[] checks = Digits.Reverse()
+                        .Select(d => d.Value)
+                        .Zip(Enumerable.Range(1,9))
+                        .Select(x => x.Item1.Value * x.Item2)
+                        .ToArray();
+            return checks.Sum() % 11 == 0;
+        }
+
+        public string Check()
+        {
+            if (Digits.Any(c => ! c.Value.HasValue))
+            {
+                return "{output}" + " ILL";
+            }
+            if (Checksum())
+            {
+                return "{output} OK";
+            }
+            else
+            {
+                return "{output} ERR";
+            }
+        }
+    }
+
+    public class OCRDigit {
+
+        public static string rawDigits = @"
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|";
+
+        public static string[] digits = OCR.Chunk(rawDigits).ToArray();
+        public static Dictionary<int, int> bitDictionary =
+            digits
+                .Select((s, i) => (s, i))
+                .ToDictionary(
+                    x => ToBitInt(ToBitArray(x.Item1)), x => x.Item2);
+
+        public string Source { get; }
+        public int? Value { get; }
+        private BitArray bitArray;
+
+        public OCRDigit(string source, int value)
+        {
+            Source = source;
+            bitArray = ToBitArray(source);
+            Value = value;
+        }
+
+        public OCRDigit(string source)
+        {
+            Source = source;
+            bitArray = ToBitArray(source);
+            Value = bitDictionary[ToBitInt(bitArray)];
+        }
+
         public static String ReadChunk(string c)
         {
             int b = ToBitInt(ToBitArray(c));
 
             return bitDictionary.ContainsKey(b) ?
                 bitDictionary[b].ToString() : "?";
-        }
-
-        public static string ReadDigits(string src)
-        {
-            var chunks = Chunk(src);
-            var ds = chunks.Select(ReadChunk);
-            return String.Concat(ds.ToArray());
-        }
-
-        public static bool Checksum(string d)
-        {
-            int[] checks = d.Reverse()
-                        .Zip(Enumerable.Range(1,9))
-                        .Select(x => Int32.Parse(x.Item1.ToString()) * x.Item2)
-                        .ToArray();
-            return checks.Sum() % 11 == 0;
-        }
-
-        public static string Check(string src)
-        {
-            string d = ReadDigits(src);
-            if (d.Any(c => c == '?'))
-            {
-                return d + " ILL";
-            }
-            if (Checksum(d))
-            {
-                return d;
-            }
-            else
-            {
-                return d + " ERR";
-            }
         }
 
         public static char[] ToBitChars(string src)
